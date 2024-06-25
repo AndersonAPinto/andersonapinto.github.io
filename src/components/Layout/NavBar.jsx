550d79918c8a0eb51b27d8187c9119b16bb087bb
AndersonAPinto <andersonaugustopinto@gmail.com>
2024-06-25 12:20:34 -0300

    update contact start page

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -4,6 +4,8 @@ import '../Layout/navBarStyleMobile.css';
 import { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md";
 import { AiOutlineAlert } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { FaWhatsapp, FaLinkedinIn, FaFacebook, FaGithub, FaInstagram, FaRegEnvelope } from "react-icons/fa";
+
 
 
 function NavBar() {
@@ -32,6 +34,12 @@ function NavBar() {
                     <button className="btn_works" >
                         <Link to={'/experience'}><p>MEUS TRABALHOS <MdOutlineKeyboardDoubleArrowDown className="icon_works" /></p></Link>
                     </button>
+                    <div style={{ display:'flex', fontSize: '30px', width: '15vw', justifyContent: 'space-around', padding: '1vh 0', height: '8vh', margin: 0,  alignItems: 'center'}}>
+                    <div className="i"><a href="https://www.linkedin.com/in/andersonapinto/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn /></a></div>
+                    <div className="i"><a href="https://github.com/AndersonAPinto" target="_blank" rel="noopener noreferrer"><FaGithub /></a></div>
+                    <div className="i"><a href="https://wa.me/5551997802755" target="_blank" rel="noopener noreferrer"><FaWhatsapp /></a></div>
+                    </div>
+                    
                 </div>
             </div>
         
